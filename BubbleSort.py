496c012af8304f0764b0f6d61773c4591d80c4fe
Bereket Fikre <118901873+bekishacode@users.noreply.github.com>
2022-12-17 17:11:09 +0300

    Sorting

---
+++ BubbleSort.py
@@ -0,0 +1,10 @@
+def countSwaps(a):
+    numSwaps = 0
+    for i in range(len(a)):
+        for j in range(len(a) - 1):
+            if a[j] > a[j + 1]:
+                a[j], a[j + 1] = a[j + 1], a[j]
+                numSwaps += 1
+    print('Array is sorted in', numSwaps, 'swaps.')
+    print('First Element:', a[0])
+    print('Last Element:', a[len(a) - 1])
